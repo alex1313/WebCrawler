386f2bed740124c5082f55297bcf827467ad99e8
alex <sav.1209@gmail.com>
2015-07-25 23:56:46 +0500

    added home controller and index view

---
+++ WebCrawler/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace WebCrawler.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
