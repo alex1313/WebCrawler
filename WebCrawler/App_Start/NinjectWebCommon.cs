bc3b21ef9cd114803f4f9f0cc3ad9450963f3630
alex <sav.1209@gmail.com>
2015-07-26 12:02:07 +0500

    installed ninject IoC

---
+++ WebCrawler/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,55 @@
+using WebActivatorEx;
+using WebCrawler;
+
+[assembly: PreApplicationStartMethod(typeof (NinjectWebCommon), "Start")]
+[assembly: ApplicationShutdownMethod(typeof (NinjectWebCommon), "Stop")]
+
+namespace WebCrawler
+{
+    using System;
+    using System.Web;
+    using DataAccess;
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper Bootstrapper = new Bootstrapper();
+
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof (OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof (NinjectHttpModule));
+            Bootstrapper.Initialize(CreateKernel);
+        }
+
+        public static void Stop()
+        {
+            Bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind(typeof (IRepository<>)).To(typeof (MongoRepository<>));
+        }
+    }
+}
